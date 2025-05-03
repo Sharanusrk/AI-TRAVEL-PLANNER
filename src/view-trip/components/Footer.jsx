a9d5d7a8cb9b141db37213eb3b7fc742e2a02011
sharanu <sharankumusagi@gmail.com>
2025-05-03 18:02:09 +0530

    Update Footer.jsx

--- src/view-trip/components/Footer.jsx
+++ src/view-trip/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='my-7 font-bold'>
-      <h2 className='text-center'>Created By <a href="https://www.linkedin.com/in/manikandan-subramanian-870656257/" className='cursor-pointer hover:text-2xl transition-all' target='_blank'>Manikandan</a></h2>
+      <h2 className='text-center'>Created By <a href="https://www.linkedin.com/in/sharanu-kumusagi" className='cursor-pointer hover:text-2xl transition-all' target='_blank'>sharanu</a></h2>
     </div>
   )
 }
