ef019c39b8437e390e0038b344056fc1f1570cce
sharanu <sharankumusagi@gmail.com>
2025-05-23 21:22:33 +0530

    Update Hero.jsx

--- src/Components/custom/Hero.jsx
+++ src/Components/custom/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover z-0"
       >
-        <source src="/new-background.mp4" type="video/mp4" />
+        <source src="/bg2.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
